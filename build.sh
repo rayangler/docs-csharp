4afa1cf7e01d12c73a405effd3b0b6aaf46b2e8d
rayangler <27821750+rayangler@users.noreply.github.com>
2024-11-19 14:24:01 -0500

    Change script location

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 if [ -f build-site.sh ]; then
   rm build-site.sh
 fi 
-curl https://raw.githubusercontent.com/mongodb/docs-worker-pool/netlify-poc/scripts/build-site.sh -o build-site.sh 
+curl https://raw.githubusercontent.com/mongodb/docs-worker-pool/refs/heads/DOP-5189-403-sharedinclude/scripts/build-site.sh -o build-site.sh 
 sh build-site.sh
